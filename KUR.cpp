59fd2f142f8ebb6de21cf63b270d71038582986d
Logan Yliniemi <lmy.shp@gmail.com>
2014-02-06 14:36:20 -0800

    KUR main file

---
+++ KUR.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   KUR.cpp
+ * Author: ylinieml
+ *
+ * Created on February 6, 2014, 2:30 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
